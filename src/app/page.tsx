24024490db267ef61a4c84063bcbf5674b201f68
Remus Poon <remuspoon@gmail.com>
2025-05-14 15:01:25 +0100

    navbar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,12 @@
-import Image from "next/image";
+import Navbar from "./_components/navbar/navbar";
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-8 p-8">
-      <div className="font-source">
-        <h1 className="text-xs font-thin">Extra Small & Thin</h1>
-        <h1 className="text-sm font-light">Small & Light</h1>
-        <h1 className="text-base font-normal">Base & Normal</h1>
-        <h1 className="text-lg font-medium">Large & Medium</h1>
-        <h1 className="text-xl font-semibold">Extra Large & Semibold</h1>
-        <h1 className="text-2xl font-bold">2XL & Bold</h1>
-        <h1 className="text-3xl font-extrabold">3XL & Extra Bold</h1>
-        <h1 className="text-4xl font-black">4XL & Black</h1>
-        <h1 className="text-5xl font-thin">5XL & Thin</h1>
-        <h1 className="text-6xl font-light">6XL & Light</h1>
-        <h1 className="text-7xl font-normal">7XL & Normal</h1>
-        <h1 className="text-8xl font-medium">8XL & Medium</h1>
-        <h1 className="text-9xl font-semibold">9XL & Semibold</h1>
-        <h1 className="italic font-bold">Italic & Bold</h1>
-        <h1 className="italic font-extrabold">Italic & Extra Bold</h1>
-        <h1 className="italic font-black">Italic & Black</h1>
-      </div>
-      <div className="font-inter">
-        <h1 className="text-xs font-thin">Extra Small & Thin</h1>
-        <h1 className="text-sm font-light">Small & Light</h1>
-        <h1 className="text-base font-normal">Base & Normal</h1>
-        <h1 className="text-lg font-medium">Large & Medium</h1>
-        <h1 className="text-xl font-semibold">Extra Large & Semibold</h1>
-        <h1 className="text-2xl font-bold">2XL & Bold</h1>
-        <h1 className="text-3xl font-extrabold">3XL & Extra Bold</h1>
-        <h1 className="text-4xl font-black">4XL & Black</h1>
-        <h1 className="text-5xl font-thin">5XL & Thin</h1>
-        <h1 className="text-6xl font-light">6XL & Light</h1>
-        <h1 className="text-7xl font-normal">7XL & Normal</h1>
-        <h1 className="text-8xl font-medium">8XL & Medium</h1>
-        <h1 className="text-9xl font-semibold">9XL & Semibold</h1>
-        <h1 className="italic font-bold">Italic & Bold</h1>
-        <h1 className="italic font-extrabold">Italic & Extra Bold</h1>
-        <h1 className="italic font-black">Italic & Black</h1>
-      </div>
+    <div className="flex flex-col items-center h-screen">
+      <Navbar />
+      <section className=""></section>
+      <section className=""></section>
+      <section className=""></section>
     </div>
   );
 }
