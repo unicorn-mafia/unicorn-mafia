143eb6fba378c33042b5538ac0e5da0294eba96c
Remus Poon <remuspoon@gmail.com>
2025-05-14 15:38:15 +0100

    Hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
+import Hero from "./_components/hero/hero";
 import Navbar from "./_components/navbar/navbar";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center h-screen">
       <Navbar />
-      <section className=""></section>
-      <section className=""></section>
-      <section className=""></section>
+      <main className="flex flex-col items-center w-full h-full px-20">
+        <Hero />
+        <section className=""></section>
+        <section className=""></section>
+      </main>
     </div>
   );
 }
