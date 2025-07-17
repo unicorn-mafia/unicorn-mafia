516a83d8c061d9d16b1ef3a380e6a89047c7eef9
Charlie Cheesman <djnr.cheesman@gmail.com>
2025-07-17 06:41:13 +0100

    udpate about

--- src/app/_components/about/about.tsx
+++ src/app/_components/about/about.tsx
@@ -6,13 +6,21 @@ export default function About() {
         <div className="w-full font-inter font-medium text-3xl tracking-tighter">about.</div>
         <div className="flex flex-col font-inter font-medium text-5xl tracking-tight w-full gap-8">
             <div className="flex flex-col gap-6 text-2xl text-gray-700">
-                <div>The highest signal community of developers in London</div>
-                <div>We do hackathons and build companies</div>
+                <div>Unicorn Mafia is an invite-only community of London's sharpest developers.</div>
+                <div className="italic">"The highest signal community of builders in London."</div>
             </div>
             <div className="flex flex-row gap-16 mt-8">
                 <div className="flex flex-col items-center">
                     <div className="font-mono text-6xl font-bold text-black">40+</div>
-                    <div className="text-xl text-gray-600">Hackathon Wins</div>
+                    <div className="text-xl text-gray-600">Hackathon wins</div>
+                </div>
+                <div className="flex flex-col items-center">
+                    <div className="font-mono text-6xl font-bold text-black">30+</div>
+                    <div className="text-xl text-gray-600">Companies being built</div>
+                </div>
+                <div className="flex flex-col items-center">
+                    <div className="font-mono text-6xl font-bold text-black">350+</div>
+                    <div className="text-xl text-gray-600">Developers</div>
                 </div>
             </div>
         </div>
